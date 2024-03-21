0c6ed3712af62836234c2b2a8c570f90cb2732f0
Abel <79184789+AbelRapha@users.noreply.github.com>
2024-03-21 01:41:35 +0000

    hidden graph function

--- functions.py
+++ functions.py
@@ -21,56 +21,55 @@ class ExperienceFunctions:
         return equation_solve
     
     def Nikuradse_experience():
+        pass
+        # fig = make_subplots(rows=2, cols=2,
+        #                     specs=[[{"secondary_y": True}, {"secondary_y": True}],
+        #                         [{"secondary_y": True}, {"secondary_y": True}]])
 
+        # # Top left
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis data"),
+        #     row=1, col=1, secondary_y=False)
 
-        fig = make_subplots(rows=2, cols=2,
-                            specs=[[{"secondary_y": True}, {"secondary_y": True}],
-                                [{"secondary_y": True}, {"secondary_y": True}]])
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis2 data"),
+        #     row=1, col=1, secondary_y=True,
+        # )
 
-        # Top left
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis data"),
-            row=1, col=1, secondary_y=False)
+        # # Top right
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis3 data"),
+        #     row=1, col=2, secondary_y=False,
+        # )
 
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis2 data"),
-            row=1, col=1, secondary_y=True,
-        )
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis4 data"),
+        #     row=1, col=2, secondary_y=True,
+        # )
 
-        # Top right
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis3 data"),
-            row=1, col=2, secondary_y=False,
-        )
+        # # Bottom left
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis5 data"),
+        #     row=2, col=1, secondary_y=False,
+        # )
 
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis4 data"),
-            row=1, col=2, secondary_y=True,
-        )
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis6 data"),
+        #     row=2, col=1, secondary_y=True,
+        # )
 
-        # Bottom left
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis5 data"),
-            row=2, col=1, secondary_y=False,
-        )
+        # # Bottom right
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis7 data"),
+        #     row=2, col=2, secondary_y=False,
+        # )
 
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis6 data"),
-            row=2, col=1, secondary_y=True,
-        )
+        # fig.add_trace(
+        #     go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis8 data"),
+        #     row=2, col=2, secondary_y=True,
+        # )
 
-        # Bottom right
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[2, 52, 62], name="yaxis7 data"),
-            row=2, col=2, secondary_y=False,
-        )
-
-        fig.add_trace(
-            go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis8 data"),
-            row=2, col=2, secondary_y=True,
-        )
-
-        st.plotly(fig)
+        # st.plotly(fig)
 
 class ConvertMeasures:
 
